ff35f9c10ad3aab1abb3281132ccfe0c3d27a373
Daniel Bustamante <danielbustamante1606@gmail.com>
2024-09-10 19:09:51 -0500

    momento1 feat creamos variables de conexion a BD

---
+++ app/database/configuration.py
@@ -0,0 +1,6 @@
+dataBaseName = "momento1"
+userName = "root"
+userPassword = ""
+connectionPort = 3306
+server = "localhost"
+dataBaseConnection = f"mysql+mysqlconnector://{userName}:{userPassword}@{server}:{connectionPort}/{dataBaseName}"
